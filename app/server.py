e65717381eae1690415b6fe22743aa1aa4226994
caiocvlopes <caiocvlopes@gmail.com>
2024-08-27 16:51:41 -0300

    feat: adding hello world blueprint

--- app/server.py
+++ app/server.py
@@ -1,15 +1,14 @@
 from sanic import Sanic
 from sanic.response import text
 from dotenv import load_dotenv
-from .config import Config
-from .listeners import setup_db
+
+from app.config import Config
+from app.listeners import setup_db
+from app.blueprints import helloworld_bp
 
 def create_app(config=Config) -> Sanic:
     load_dotenv()
     app = Sanic("MyHelloWorldApp", config())
     app.register_listener(setup_db, "before_server_start")
-
-    @app.get("/")
-    async def hello_world(request):
-        return text("Hello, world.")
+    app.blueprint(helloworld_bp)
     return app
