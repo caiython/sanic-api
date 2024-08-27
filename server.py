fe409d748e58cdf2c5392f68e910c52fdb9029c1
caiocvlopes <caiocvlopes@gmail.com>
2024-08-27 14:58:39 -0300

    feat: applying factory pattern to server file

--- server.py
+++ server.py
@@ -3,12 +3,14 @@ from sanic.response import text
 import aiosqlite
 from config import Config
 
-app = Sanic("MyHelloWorldApp", Config())
+def create_app(config=Config) -> Sanic:
+    app = Sanic("MyHelloWorldApp", config())
 
-@app.before_server_start
-async def attach_db(app, loop):
-    app.ctx.db = await aiosqlite.connect(app.config.DB_PATH)
+    @app.before_server_start
+    async def attach_db(app, loop):
+        app.ctx.db = await aiosqlite.connect(app.config.DB_PATH)
 
-@app.get("/")
-async def hello_world(request):
-    return text("Hello, world.")
+    @app.get("/")
+    async def hello_world(request):
+        return text("Hello, world.")
+    return app
